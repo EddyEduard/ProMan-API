6ca0469c9395497c55d36d9448d8bb8e13ae848e
eduard_nicolae <eduard_nicolae@yahoo.com>
2021-12-24 15:31:36 +0200

    Token model

---
+++ src/main/java/com/team/proman/model/TokenModel.java
@@ -0,0 +1,53 @@
+package com.team.proman.model;
+
+import java.util.Date;
+
+public class TokenModel {
+	private String token;
+	
+	private String username;
+	
+	private Date expiration;
+	
+	/**
+	 * @return the token
+	 */
+	public String getToken() {
+		return token;
+	}
+
+	/**
+	 * @param token the token to set
+	 */
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the expiration
+	 */
+	public Date getExpiration() {
+		return expiration;
+	}
+
+	/**
+	 * @param expiration the expiration to set
+	 */
+	public void setExpiration(Date expiration) {
+		this.expiration = expiration;
+	}
+}
