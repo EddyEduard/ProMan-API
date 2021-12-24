8a032c4884d78a0a4567f7a711e33b78b3319b58
eduard_nicolae <eduard_nicolae@yahoo.com>
2021-12-24 15:30:11 +0200

    Main class

--- src/main/java/com/team/proman/PromanApplication.java
+++ src/main/java/com/team/proman/PromanApplication.java
@@ -2,20 +2,12 @@ package com.team.proman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-@RestController
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PromanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PromanApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 }
