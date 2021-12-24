5aadc109f0c99e63ab8346ee9198225fb81454c2
eduard_nicolae <eduard_nicolae@yahoo.com>
2021-12-24 15:35:02 +0200

    JSON Web Token authentication entry point

---
+++ src/main/java/com/team/proman/component/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.team.proman.component;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final long serialVersionUID = 205968776469287325L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
+}
