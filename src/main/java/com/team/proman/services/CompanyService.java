e84bfa2d5e7e9c1359b9921ff7d138caec4fca35
eduard_nicolae <eduard_nicolae@yahoo.com>
2022-01-11 12:34:43 +0200

    Company service

--- src/main/java/com/team/proman/services/CompanyService.java
+++ src/main/java/com/team/proman/services/CompanyService.java
@@ -1,5 +1,7 @@
 package com.team.proman.services;
 
+import java.util.Iterator;
+
 import org.springframework.stereotype.Service;
 
 import com.team.proman.model.db.Company;
@@ -25,5 +27,53 @@ public class CompanyService {
 	 */
 	public Company create(Company company) {
 		return companyRepository.save(company);
+	}
+	
+	/**
+	 * Update a company by id.
+	 * 
+	 * @param id
+	 * @param account
+	 * @return updated account
+	 */
+	public Company update(Long id, Company company) {
+		Company foundCompany = findById(id);
+		foundCompany.setName(company.getName());
+		foundCompany.setEmail(company.getEmail());
+		foundCompany.setPhone(company.getPhone());
+		foundCompany.setIndustry(company.getIndustry());
+		foundCompany.setCountry(company.getCountry());
+		foundCompany.setCity(company.getCity());
+		foundCompany.setAddress(company.getAddress());
+
+		return companyRepository.save(foundCompany);
+	}
+	
+	/**
+	 * Delete a company profile.
+	 * 
+	 * @param company
+	 */
+	public void delete(Company company) {
+		companyRepository.delete(company);
+	}
+	
+	/**
+	 * Find a company by id.
+	 * 
+	 * @param id
+	 * @return found company or null
+	 */
+	public Company findById(Long id) {
+		Iterator<Company> companies = companyRepository.findAll().iterator();
+
+		while (companies.hasNext()) {
+			Company company = companies.next();
+			if (company.getId().equals(id)) {
+				return company;
+			}
+		}
+
+		return null;
 	}
 }
