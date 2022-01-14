3a02ee0118bd5a2c56e54b1d6cf757d23e696bbe
eduard_nicolae <eduard_nicolae@yahoo.com>
2022-01-14 15:10:39 +0200

    Sprint model

---
+++ src/main/java/com/team/proman/model/SprintModel.java
@@ -0,0 +1,105 @@
+package com.team.proman.model;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.team.proman.model.db.Sprint;
+import com.team.proman.model.enums.Priority;
+import com.team.proman.model.enums.Status;
+
+public class SprintModel {
+	private Long projectId;
+	
+	@NotBlank(message = "The name is required.")
+	@NotNull(message = "The name cannot be null.")
+	@NotEmpty(message = "The name cannot be empty.")
+	@Size(min = 6, max = 50, message = "The length of name must be between 6 and 250 characters.")
+	private String name;
+
+	private String description;
+
+	private Priority priority;
+
+	private Status status;
+
+	public Sprint getSprint(Long companyId, Status status, Date created_date,
+			Date updated_date) {
+		return new Sprint(companyId, this.projectId, this.name, this.description, this.priority, status, created_date,
+				updated_date);
+	}
+
+	/**
+	 * @return the projectId
+	 */
+	public Long getProjectId() {
+		return projectId;
+	}
+
+
+	/**
+	 * @param projectId the projectId to set
+	 */
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the priority
+	 */
+	public Priority getPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(Priority priority) {
+		this.priority = priority;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public Status getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+}
