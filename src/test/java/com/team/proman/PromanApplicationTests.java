212001815c0c3b73a0c2eaf8bab4002a36238c0a
eduard_nicolae <eduard_nicolae@yahoo.com>
2022-03-04 18:46:47 +0200

    Main class

--- src/test/java/com/team/proman/PromanApplicationTests.java
+++ src/test/java/com/team/proman/PromanApplicationTests.java
@@ -1,13 +1,8 @@
 package com.team.proman;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PromanApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
